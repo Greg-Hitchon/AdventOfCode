2200010df85b90c4e9906f1787ab858d7a959775
ghitchon <ghitchon@corrosionservice.com>
2022-12-06 16:23:03 -0500

    Day 4

--- AdventOfCode/Challenge.cs
+++ AdventOfCode/Challenge.cs
@@ -6,39 +6,19 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
+
+   
     public static class Challenge
     {  
-        private static int GetPriority(string sack1, string sack2, string sack3)
-        {
-            var sack1Lookup = sack1.ToList();
-            var sack2Lookup = sack2.ToList();
-            var sack3Lookup = sack3.ToList();
-
-            var key = sack1Lookup.FirstOrDefault(x => sack2Lookup.Contains(x) && sack3Lookup.Contains(x)).ToString();
-
-            if(key.ToUpper() == key)
-            { // upper
-                return char.Parse(key) - 38;
-            }
-            else
-            {
-                return char.Parse(key) - 96;
-            }
-        }
-
         public static int GetResult()
         {
             List<string> inputLines = File.ReadAllLines(Path.Combine(Globals.RootInputPath, "Input.txt")).ToList();
-            var input = inputLines.ToList();
+            var input = inputLines.Select(x=>new Tuple<List<int>, List<int>>(
+                Enumerable.Range(int.Parse(x.Split(",")[0].Split("-")[0]), int.Parse(x.Split(",")[0].Split("-")[1]) - int.Parse(x.Split(",")[0].Split("-")[0]) + 1).ToList(),
+                Enumerable.Range(int.Parse(x.Split(",")[1].Split("-")[0]), int.Parse(x.Split(",")[1].Split("-")[1]) - int.Parse(x.Split(",")[1].Split("-")[0]) + 1).ToList()
+            )).Where(x=>x.Item1.Any(x1 => x.Item2.Contains(x1)) || x.Item2.Any(x1 => x.Item1.Contains(x1))).ToList();
 
-            List<int> priorities = new List<int>();
-            for (int i = 0; i < input.Count; i+=3)
-            {
-                priorities.Add(GetPriority(input[i], input[i + 1], input[i + 2]));
-            }
-
-
-            return priorities.Sum();
+            return input.Count;
         }
     }
 }
