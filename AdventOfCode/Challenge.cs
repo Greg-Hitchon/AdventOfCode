b178611db16ed57d6a19b731aee4e055aa7b0e8e
ghitchon <ghitchon@corrosionservice.com>
2022-12-08 10:35:44 -0500

    Day 8

--- AdventOfCode/Challenge.cs
+++ AdventOfCode/Challenge.cs
@@ -9,43 +9,89 @@ namespace AdventOfCode
 
    
     public static class Challenge
-    {  
+    {
+
+        public static List<int> Up(int[,] input, int i, int j)
+        {
+            List<int> result = new List<int>();
+            for (int k = i-1; k >= 0 ; k--)
+            {
+                result.Add(input[k, j]);
+            }
+            return result;
+        }
+
+        public static List<int> Down(int[,] input, int i, int j)
+        {
+            List<int> result = new List<int>();
+            for (int k = i+1; k < input.GetLength(0); k++)
+            {
+                result.Add(input[k, j]);
+            }
+            return result;
+        }
+
+        public static List<int> Left(int[,] input, int i, int j)
+        {
+            List<int> result = new List<int>();
+            for (int k = j - 1; k >= 0; k--)
+            {
+                result.Add(input[i, k]);
+            }
+            return result;
+        }
+
+        public static List<int> Right(int[,] input, int i, int j)
+        {
+            List<int> result = new List<int>();
+            for (int k = j + 1; k < input.GetLength(1); k++)
+            {
+                result.Add(input[i, k]);
+            }
+            return result;
+        }
+
+        public static int GetScore(List<int> heights, int current)
+        {
+            if (heights.Count == 0) return 0;
+            int temp = heights.TakeWhile(x => x < current).Count();
+            return temp + (temp != heights.Count ? 1 : 0);
+        }
 
         public static int GetResult()
         {
             List<string> inputLines = File.ReadAllLines(Path.Combine(Globals.RootInputPath, "Input.txt")).ToList();
             //string input = File.ReadAllText(Path.Combine(Globals.RootInputPath, "Input.txt"));
 
-
-            var results = new Dictionary<string, int>();
-            List<string> currentPaths = new List<string>();
-
-            foreach (var line in inputLines)
+            int[,] input = new int[inputLines.Count, inputLines[0].Length];
+            for (int i = 0; i < inputLines.Count; i++)
             {
-                if (line.StartsWith("$ cd ") && !line.Contains("."))
+                for (int j = 0; j < inputLines[i].AsEnumerable().Count(); j++)
                 {
-                    string currentPath = string.Join("//", currentPaths) + "//" + $"{line.Split(" ")[2]}";
-                    currentPaths.Add(currentPath);
-                    results.Add(currentPath, 0);
+                    input[i, j] = int.Parse(inputLines[i][j].ToString());
                 }
-                else if(line == "$ cd ..")
+            }
+
+            int maxScore = int.MinValue;
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
                 {
-                    currentPaths.RemoveAt(currentPaths.Count - 1);
-                }
-                else if(!line.StartsWith("dir") && !line.StartsWith("$"))
-                {
-                    int size = int.Parse(line.Split(" ")[0]);
-                    foreach (var path in currentPaths)
+                    int currentHeight = input[i, j];
+                    int score = GetScore(Left(input, i, j), currentHeight) *
+                        GetScore(Right(input, i, j), currentHeight) *
+                        GetScore(Up(input, i, j), currentHeight) *
+                        GetScore(Down(input, i, j), currentHeight);
+
+                    if (score > maxScore)
                     {
-                        results[path] += size;
+                        maxScore = score;
                     }
                 }
             }
 
-            int used = results["///"];
-            int needed = used - 40000000;
 
-            return results.Select(x => x.Value).OrderBy(x => x).FirstOrDefault(x => x >= needed);
+            return maxScore;
         }
     }
 }
